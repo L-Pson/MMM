a04b2e1644eb8cbcf41e563d34f75e5a8960e3d1
L-PSon <104149942+L-Pson@users.noreply.github.com>
2023-11-15 17:05:14 +0000

    V2 initialisation

--- README.md
+++ README.md
@@ -1,9 +1,2 @@
 # MMM
 Management tool for headless interfaces streamed via Moonlight.
-This is a second example.
-
-This is an example written directly to github.com.
-
-
-Here we have a bit of a conflict!!
-This is a conflicting example!  
--- batch files/Work laptop 2nd monitor 1.ps1
+++
@@ -1,6 +0,0 @@
-$parent = (get-item $PSScriptRoot ).parent.FullName
-$python_path = Get-Content -Path $parent/env/python_path
-$main_path = $parent + "\main.py"
-
-$cmd = "& `"{0}`" `"{1}`" moonlight_secondary_monitor 1 wall_mounted_and_work_laptop" -f $python_path, $main_path
-Invoke-Expression $cmd
